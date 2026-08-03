8313418cb6e8e3e2fbd6948b251a088f1b193cf9
Muwanguzi samson <muwanguzi@necxa.uk>
2026-08-03 05:10:42 +0000

    Initialize widget binding in theme tests

--- test/theme_test.dart
+++ test/theme_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:necxa_flutter/theme.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   tearDown(() => C.themeMode = ThemeMode.system);
 
   test('light mode uses the soft-white palette', () {
